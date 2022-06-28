0c129fb668971208cc0ccf333b01151ef8006a99
ivolarevic <46626092+ivolarevic@users.noreply.github.com>
2022-06-28 13:09:33 +0200

    fix

--- app/src/main/java/com/example/tindroom/ui/postlogin/ChatFragment.java
+++ app/src/main/java/com/example/tindroom/ui/postlogin/ChatFragment.java
@@ -15,6 +15,8 @@ import com.example.tindroom.data.model.User;
 import com.example.tindroom.network.RetrofitService;
 import com.example.tindroom.network.TindroomApiService;
 import com.example.tindroom.utils.LoadingDialogBar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -36,17 +38,37 @@ public class ChatFragment extends Fragment {
     View rootView;
     private TindroomApiService tindroomApiService;
     private User sessionUser;
-    static ArrayList<Swipe> swipes = new ArrayList<>();
-    static ArrayList<User> chatUsers = new ArrayList<>();
+    static ArrayList<Swipe> swipes;
+    static ArrayList<User> chatUsers;
     private RecyclerView recyclerView;
     private ChatAdapter.RecyclerViewClickListener listener;
     LoadingDialogBar loadingDialogBar;
+
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         rootView = inflater.inflate(R.layout.fragment_chat, container, false);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+
+        Retrofit retrofit = RetrofitService.getRetrofit();
+        tindroomApiService = retrofit.create(TindroomApiService.class);
+        sessionUser = SharedPreferencesStorage.getSessionUser(requireContext());
+        loadingDialogBar = new LoadingDialogBar(getActivity());
+
+        chatUsers = new ArrayList<>();
+        swipes  = new ArrayList<>();
+
+        getSwipes();
+        initViews();
+        initListeners();
+        setOnClickListener();
+
         return rootView;
 
     }
@@ -54,21 +76,7 @@ public class ChatFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable final Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        Retrofit retrofit = RetrofitService.getRetrofit();
-        tindroomApiService = retrofit.create(TindroomApiService.class);
-        sessionUser = SharedPreferencesStorage.getSessionUser(requireContext());
-        loadingDialogBar = new LoadingDialogBar(getActivity());
 
-        getSwipes();
-        initViews();
-        initListeners();
-        Log.d("chatusers", chatUsers.toString());
-        setOnClickListener();
-
-        ChatAdapter chatAdapter = new ChatAdapter(ChatFragment.this, chatUsers, listener);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false);
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setAdapter(chatAdapter);
 
     }
     private void initViews() {
@@ -76,11 +84,9 @@ public class ChatFragment extends Fragment {
     }
 
     private void initListeners() {
-
     }
 
     private void getSwipes() {
-        loadingDialogBar.startLoadingDialog();
         Call<List<Swipe>> usersSwipesCall = tindroomApiService.getUsersSwipes(sessionUser.getUserId());
         usersSwipesCall.enqueue(new Callback<List<Swipe>>() {
             @Override
@@ -96,48 +102,49 @@ public class ChatFragment extends Fragment {
         });
     }
 
-    private ArrayList<User> addData(List<Swipe> data){
+    private void addData(List<Swipe> data){
         swipes.addAll(data);
+        User user = new User();
+        String id = "";
+
         if(!swipes.isEmpty()){
             for(Swipe swipe : swipes){
-                User user = new User();
                 if(swipe.isSwipe_1() != null && swipe.isSwipe_2() != null && swipe.isSwipe_1() && swipe.isSwipe_2() ){
                     if(swipe.getUserId1().equals(sessionUser.getUserId())){
                         user.setUserId(swipe.getUserId2());
+                        id = swipe.getUserId2();
                     }else{
                         user.setUserId(swipe.getUserId1());
+                        id = swipe.getUserId1();
                     }
-                    addChatUser(user.getUserId());
+
+                    addChatUser(id);
                 }
             }
+        }else{
+            Log.d("nema", swipes.toString());
         }
-        Log.d("!!!", chatUsers.toString());
-        return chatUsers;
     }
 
     private void addChatUser(String id){
-            Log.d("usao u if", chatUsers.toString());
             Call<User> userIdCall = tindroomApiService.getUserById(id);
             userIdCall.enqueue(new Callback<User>() {
                 @Override
                 public void onResponse(Call<User> call, Response<User> response) {
-                    loadingDialogBar.dismissDialog();
                     if(response.body() != null){
-                        User user = new User();
-                        user = response.body();
-                        if(!chatUsers.contains(user)){
-                            chatUsers.add(response.body());
-                        }
+                        addChatUsers(response.body());
                     }
                 }
                 @Override
                 public void onFailure(Call<User> call, Throwable t) {
                     Toast.makeText(getContext(), getResources().getString(R.string.unexpected_error_occurred), Toast.LENGTH_SHORT).show();
-                    loadingDialogBar.dismissDialog();
                 }
             });
             Log.d("chatuserss", chatUsers.toString());
-//        addAdapter();
+    }
+    private void addChatUsers(User chatUser){
+        chatUsers.add(chatUser);
+        addAdapter();
     }
 
     private void addAdapter(){
